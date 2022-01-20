c1d3d41dc53d12c062171b5c7f5ba79167f7cf84
navanee99 <navaneenagarajan305@gmail.com>
2022-01-20 11:47:13 +0530

    flatlaff added

--- src/main/java/com/vit/restaurant/App.java
+++ src/main/java/com/vit/restaurant/App.java
@@ -1,4 +1,6 @@
 package com.vit.restaurant;
+import javax.swing.*;
+import com.formdev.flatlaf.*;
 
 /**
  * Hello world!
@@ -12,6 +14,12 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+
         System.out.println("Hello World java guys..hi!");
     }
 }
